da5459bcd8bcd0757da3d0a1b9091d6a938cf07d
齋藤健悟 <k.saitoh0526@gmail.com>
2019-12-14 16:30:28 +0900

    二つ目顔を示すための四角を表示

--- FaceTracker/FaceTracker/ViewController.swift
+++ FaceTracker/FaceTracker/ViewController.swift
@@ -10,17 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var faceTracker:FaceTracker? = nil;
-    @IBOutlet var cameraView :UIView!//viewController上に一つviewを敷いてそれと繋いでおく
+    var faceTracker: FaceTracker? = nil;
+    //viewController上に一つviewを敷いてそれと繋いでおく
+    @IBOutlet var cameraView :UIView!
 
-    var rectView = UIView()
+    var rectViewFront = UIView()
+    var rectViewBack = UIView()
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.rectView.layer.borderWidth = 3//四角い枠を用意しておく
-        self.view.addSubview(self.rectView)
+        
+        self.rectViewFront.layer.borderWidth = 3
+        self.rectViewBack.layer.borderWidth = 3
+        rectViewBack.layer.borderColor = UIColor.red.cgColor
+        self.view.addSubview(self.rectViewFront)
+        view.addSubview(rectViewBack)
+        
         faceTracker = FaceTracker(view: self.cameraView, findface:{arr in
-            let rect = arr[0];//一番の顔だけ使う
-            self.rectView.frame = rect;//四角い枠を顔の位置に移動する
+            print(arr.count)
+            if arr.count < 1 {
+                self.rectViewFront.isHidden = true
+                self.rectViewBack.isHidden = true
+            }
+            if arr.indices.contains(0) {
+                self.rectViewFront.isHidden = false
+                //一番の顔だけ使う
+                let rect1 = arr[0]
+                //四角い枠を顔の位置に移動する
+                self.rectViewFront.frame = rect1
+            }
+            
+            if arr.indices.contains(1) {
+                self.rectViewBack.isHidden = false
+                let rect2 = arr[1]
+                self.rectViewBack.frame = rect2
+            } else {
+                self.rectViewBack.isHidden = true
+            }
         })
     }
 
