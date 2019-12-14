3bb411ffda7129dc90c7fd49cf803f5735bfa890
shinji46kuro <shinji.46.kuro@gmail.com>
2019-12-15 01:34:04 +0900

    画面にじゃがいもを複製するところまで

--- FaceTracker/FaceTracker/ViewController.swift
+++ FaceTracker/FaceTracker/ViewController.swift
@@ -13,11 +13,14 @@ import SceneKit
 class ViewController: UIViewController {
 
     var faceTracker: FaceTracker? = nil;
-    @IBOutlet var cameraView :UIView!
+    @IBOutlet var cameraView: UIView!
     @IBOutlet weak var duplicatedCameraView: UIView!
 
     var eggViews = [UIView]()
     var rectList = [CGRect]()
+    
+    var duplicatedEggViews = [UIView]()
+    var duplicatedRectList = [CGRect]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,22 +32,29 @@ class ViewController: UIViewController {
                              repeats: true)
         
         faceTracker = FaceTracker(view: self.cameraView, duplicatedView: duplicatedCameraView) { [weak self] rectList in
+            guard let self = self else { return }
             // 数が変わったときはリスト自体を再作成
-            guard (self?.rectList.count ?? 0) == rectList.count else {
-                self?.rectList = rectList
+            guard self.rectList.count == rectList.count else {
+                self.rectList = rectList
                 return
             }
             
             // 数が変わらないときは、近いリストのRectを変更する
+            let cameraHeight = self.cameraView.frame.height
             rectList.forEach({ rect in
-                let nearestResultIndex = self?.rectList.enumerated().reduce((0, 10000)) { result, anotherRect -> (Int, CGFloat) in
-                    let rectDistance = self?.distance(from: anotherRect.element.origin, to: rect.origin) ?? 10000 
+                let nearestResultIndex = self.rectList.enumerated().reduce((0, 10000)) { result, anotherRect -> (Int, CGFloat) in
+                    let rectDistance = self.distance(from: anotherRect.element.origin, to: rect.origin) 
                     return rectDistance <= result.1 ? (anotherRect.offset, rectDistance) : result
-                }.0 ?? 0
-                guard (self?.eggViews.count ?? 0) > nearestResultIndex else {
-                    return
-                } 
-                self?.eggViews[nearestResultIndex].frame = rect
+                }.0
+                
+                guard self.eggViews.count > nearestResultIndex else { return } 
+                self.eggViews[nearestResultIndex].frame = rect
+                
+                guard self.duplicatedEggViews.count > nearestResultIndex else { return } 
+                self.duplicatedEggViews[nearestResultIndex].frame = CGRect(x: rect.origin.x, 
+                                                                           y: rect.origin.y + cameraHeight,
+                                                                           width: rect.width,
+                                                                           height: rect.height)
             })
         }
     }
@@ -62,7 +72,7 @@ class ViewController: UIViewController {
             self.eggViews.forEach { $0.removeFromSuperview() }
             self.eggViews = []
             rectList.forEach { self.appendEggView(in: $0) }
-            
+            duplicateEggViews()
         } else if eggViews.count == rectList.count {
             // TODO: 値がそのままのときは、Viewを合わせていく
         } else {
@@ -70,10 +80,8 @@ class ViewController: UIViewController {
             // 足りない分のViewは追加する
             self.eggViews.forEach { $0.removeFromSuperview() }
             self.eggViews = []
-            
-            rectList.forEach {
-                self.appendEggView(in: $0)
-            }
+            rectList.forEach { self.appendEggView(in: $0) }
+            duplicateEggViews()
         }
     }
     
@@ -81,18 +89,43 @@ class ViewController: UIViewController {
         let containerView = UIView(frame: rect)
         view.addSubview(containerView)
         
-        let eggView = SCNView()
-        eggView.scene = SCNScene(named: "SceneKitAsset.scnassets/poteto.scn")!
-        eggView.backgroundColor = .clear
+        let eggView = makeEggView()
         containerView.addSubview(eggView)
         eggView.fillSuperview()
         
         eggViews.append(containerView)
     }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+    
+    private func duplicateEggViews() {
+        duplicatedRectList = []
+        duplicatedEggViews.forEach { $0.removeFromSuperview() }
+        duplicatedEggViews = []
+        
+        rectList.forEach {
+            let y = $0.origin.y + cameraView.frame.height
+            duplicatedRectList.append(CGRect(x: $0.origin.x, y: y, width: $0.size.width, height: $0.size.height))
+        }
+        duplicatedRectList.forEach { 
+            appendDuplicatedEggView(in: $0)
+        }
     }
     
+    private func appendDuplicatedEggView(in rect: CGRect) {
+        let containerView = UIView(frame: rect)
+        view.addSubview(containerView)
+        
+        let eggView = makeEggView()
+        containerView.addSubview(eggView)
+        eggView.fillSuperview()
+        
+        duplicatedEggViews.append(containerView)
+    }
+    
+    private func makeEggView() -> UIView {
+        let eggView = SCNView()
+        eggView.scene = SCNScene(named: "SceneKitAsset.scnassets/poteto.scn")!
+        eggView.backgroundColor = .clear
+        return eggView
+    }    
 }
 
